d4369e77ab1286a8b8107d22221f42d861f47cfa
abiola <abiola.kayode@gmail.com>
2021-06-16 17:11:46 +0000

    Commit

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const express = require("express")
+const mongoose = require('mongoose');
+const session = require("express-session")
+const redis = require ("redis")
+let RedisStore = require("connect-redis")(session)
 
-const app = express()
 
-app.get("/", (req, res) =>
+const { MONGO_USER, MONGO_PASSWORD, MONGO_PORT, MONGO_IP, REDIS_URL, SESSION_SECRET, REDIS_PORT } = require("./config/config");
+
+let redisClient = redis.createClient({
+  host:REDIS_URL,
+  port: REDIS_PORT,
+})
+const postRouter = require("./routes/postRoutes")
+
+const userRouter = require("./routes/userRoutes")
 
 
-    res.send("<h2> Hi there </h2>")
+const app = express();
 
-)
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+mongoose.connect(mongoURL)
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e));
+
+app.use(session({
+  store: new RedisStore({client: redisClient}),
+  secret: SESSION_SECRET,
+  cookie: {
+    secure: false,
+    resave: false,
+    saveUninitialized: false,
+    httpOnly: true,
+    maxAge: 30000, //milliseconds
+  }
+}))
+
+app.use(express.json());
+app.get("/", (req, res) => {
+
+
+  res.send("<h2> Hi there</h2>")
+
+});
+
+//localhost:3000/api/v1/post
+
+app.use("/api/v1/posts", postRouter);
+
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 3000
 
